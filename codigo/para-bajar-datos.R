6258505500d3a16a4a8f1ab8e7e0400e08c292a5
nachalca <nachalca@iesta.edu.uy>
2023-09-04 14:56:52 -0300

    serie precipitaciones

---
+++ codigo/para-bajar-datos.R
@@ -0,0 +1,27 @@
+
+Variables 
+Surface downwelling shortwave radiation (esta mensual)
+
+Diarias
+precipitacion 
+Near-surface air temperature
+Near-surface wind speed
+Near-surface specific humidity
+
+capaz
+Daily maximum near-surface air temperature
+Daily minimum near-surface air temperature
+
+
+Esenarios: 
+historicos
+
+
+Modelos:
+
+Periodo
+Historícos: 2000 - 2050
+
+
+region:
+notre -30, east -52, sur -35, oeste -58
