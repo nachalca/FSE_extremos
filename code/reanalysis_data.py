266c988e879663b2b75f67e35b78439da0be4a27
tancre <brunotancredi00@gmail.com>
2024-08-05 12:02:48 -0300

    update script

--- code/reanalysis_data.py
+++ code/reanalysis_data.py
@@ -60,19 +60,19 @@ VARIABLES = {
     "total_cloud_cover": {
         "reanalysis_name": "tcc", 
         "cmip6_name":"clt",
-        "hourly": False,
+        "hourly": True,
         "need_to_transform": True, #We need to multiply by 100
     },   
     "toa_incident_solar_radiation": {
         "reanalysis_name": "tisr", 
         "cmip6_name":"rsdt",
-        "hourly": False,
+        "hourly": True,
         "need_to_transform": True, # We need to transform Joules to Watts, so we need to divide by the amount of seconds 60*60*24
     },
     "surface_solar_radiation_downwards": {
         "reanalysis_name": "ssrd", 
         "cmip6_name":"rsds",
-        "hourly": False,
+        "hourly": True,
         "need_to_transform": True, # We need to transform Joules to Watts, so we need to divide by the amount of seconds 60*60*24
    },
 }
@@ -94,7 +94,6 @@ def download_data(variable):
     for year in YEARS:
         #If the file already exists, do not download it
         if not os.path.exists(f"{year}.nc"):
-            if(VARIABLES.get(variable).get("hourly")):
                 CDS.retrieve(
                     'reanalysis-era5-single-levels',
                     {
@@ -134,36 +133,7 @@ def download_data(variable):
                         ],
                     },
                     f"{year}.nc")
-            else:
-                CDS.retrieve(
-                    'reanalysis-era5-single-levels',
-                    {
-                        'variable': variable,
-                        'year': year,
-                        'area': AREA,
-                        'format': 'netcdf',
-                        'product_type': 'reanalysis',       
-                        'month': [
-                            '01', '02', '03',
-                            '04', '05', '06',
-                            '07', '08', '09',
-                            '10', '11', '12',
-                        ],
-                        'day': [
-                            '01', '02', '03',
-                            '04', '05', '06',
-                            '07', '08', '09',
-                            '10', '11', '12',
-                            '13', '14', '15',
-                            '16', '17', '18',
-                            '19', '20', '21',
-                            '22', '23', '24',
-                            '25', '26', '27',
-                            '28', '29', '30',
-                            '31',
-                        ],
-                    },
-                    f"{year}.nc")
+
     os.chdir(ROOT_FOLDER)
 
             
@@ -209,33 +179,6 @@ def summarize_data(variable):
     os.remove(f"{variable_name}.nc") #drop the nc file to realese space
     os.chdir(ROOT_FOLDER)
 
-#A function to expand the daily variables to a hourly scale using the csv file obtained in summarize_data
-def expand_daily_to_hourly(variable):
-    print(f"Expanding the daily data to an hourly scale for variable \033[92m{variable}\033[0m")
-    os.chdir(ROOT_FOLDER)
-    variable_name = VARIABLES.get(variable).get("reanalysis_name")
-    os.chdir(f"{ROOT_FOLDER}/data/reanalysis/reanalysis-{variable_name}")
-    data = pd.read_csv(f"{variable_name}.csv")
-
-    #Substract 12 hr to time column, idk but the time is setted to 12:00 so we need to change it to 00:00
-    data['time'] = pd.to_datetime(data['time']) - timedelta(hours=12)
-    data = data.set_index('time')
-
-    # Append an extra day with NaN value to handle the last day edge case
-    last_day = data.index[-1] + pd.Timedelta(days=1)
-    data.loc[last_day] = [1]
-    
-    data = data.resample('h').ffill()
-    
-    # Remove the extra day
-    data = data[data.index < last_day]
-
-    #Assert that time has the correct size
-    days_between = (datetime(YEARS[-1] + 1,1,1) - datetime(YEARS[0], 1, 1)).days
-    assert days_between * 24 == len(data), f"Time has the wrong size: {len(data)}, when it should be {days_between * 24}"
-    data.to_csv(f"{variable_name}.csv")
-    os.chdir(ROOT_FOLDER)
-
 
 # A function to merge all the dataframes
 def merge_all_data():
@@ -269,37 +212,34 @@ def final_dataset():
     #total_precipitation column needs to be transformed to kg m-2 s-1
     data['pr'] = data['pr'] * 1000 / 3600
     #We need to take the module of the wind
-    data['sfcWind'] = (data['uas']**2 + data['vas']**2)**0.5
+    data['sfcWind'] = data['uas']
     data = data.drop(columns=['uas', 'vas'])
     #We need to transform the cloud cover to percentage
     data['clt'] = data['clt']*100
     #We need to transform the toa_incident_solar_radiation to W m-2
-    data['rsdt'] = data['rsdt'] / (60*60*24)
+    data['rsdt'] = data['rsdt'] / (60*60)
     #We need to transform the solar radiation to W m-2
-    data['rsds'] = data['rsds'] / (60*60*24)
+    data['rsds'] = data['rsds'] / (60*60)
 
     #Get the solar position.
-    data['time_2'] = pd.to_datetime(data['time'])
-    print("Getting the solar position, it may take some time")
-    data[['elevation', 'azimuth']] = data['time_2'].apply(lambda x: pd.Series(sun_position(x)))    #Add the solar position
-
-    data.drop(columns=['time_2'], inplace=True)
+#    data['time_2'] = pd.to_datetime(data['time'])
+#    print("Getting the solar position, it may take some time")
+#    data[['elevation', 'azimuth']] = data['time_2'].apply(lambda x: pd.Series(sun_position(x)))    #Add the solar position
+#    data.drop(columns=['time_2'], inplace=True)
 
     data.to_csv("data/reanalysis/reanalysis.csv", index=False)
 
 
 def main():
 #    Download the data for each variable
-    for variable in VARIABLES:
-        try:
-            download_data(variable)
-            join_files(variable)
-            summarize_data(variable)
-            if not VARIABLES.get(variable).get("hourly"):
-                expand_daily_to_hourly(variable)
+    # for variable in VARIABLES:
+    #     try:
+    #         download_data(variable)
+    #         join_files(variable)
+    #         summarize_data(variable)
 
-        except Exception as e:
-            print(f"\033[91mError with variable {variable}: {e}\033[0m")
+    #     except Exception as e:
+    #         print(f"\033[91mError with variable {variable}: {e}\033[0m")
     
     merge_all_data()
     final_dataset()
