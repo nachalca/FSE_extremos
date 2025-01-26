f53adea8ce4a2b9917acf57b829738f0293b3e84
tancre <brunotancredi00@gmail.com>
2025-01-25 22:14:20 -0300

    update brazil

--- code/reanalysis_data.py
+++ code/reanalysis_data.py
@@ -2,10 +2,10 @@
 This script downloads the ERA5 reanalysis data from the Copernicus Climate Data Store (CDS) using the cdsapi library.
 Also transform the data.
 """
-
 import cdsapi
 import os
 import pandas as pd
+import numpy as np
 import xarray as xr
 import yaml    
 from datetime import datetime, timedelta
@@ -53,68 +53,99 @@ def download_data(variable, variable_name):
     if not os.path.exists(f"{variable_name}.nc"):
         #Download the data in 10 years batches
         for i in range(0, len(YEARS), 10):
-            CDS.retrieve(
-                'reanalysis-era5-single-levels',
-                {
-                    'variable': variable,
-                    'year': YEARS[i:min(i+10,len(YEARS))],
-                    'area': AREA,
-                    'format': 'netcdf',
-                    'product_type': ['reanalysis'],  
-                    "download_format": "unarchived",    
-                    'month': [
-                        '01', '02', '03',
-                        '04', '05', '06',
-                        '07', '08', '09',
-                        '10', '11', '12',
-                    ],
-                    'day': [
-                        '01', '02', '03',
-                        '04', '05', '06',
-                        '07', '08', '09',
-                        '10', '11', '12',
-                        '13', '14', '15',
-                        '16', '17', '18',
-                        '19', '20', '21',
-                        '22', '23', '24',
-                        '25', '26', '27',
-                        '28', '29', '30',
-                        '31',
-                    ],
-                    'time': [
-                        '00:00', '01:00', '02:00',
-                        '03:00', '04:00', '05:00',
-                        '06:00', '07:00', '08:00',
-                        '09:00', '10:00', '11:00',
-                        '12:00', '13:00', '14:00',
-                        '15:00', '16:00', '17:00',
-                        '18:00', '19:00', '20:00',
-                        '21:00', '22:00', '23:00',
-                    ]
-                },
-                f"{variable_name}_part{i}.nc")
+            if not os.path.exists(f"{variable_name}_part{i//10 + 1}.nc"):
+                CDS.retrieve(
+                    'reanalysis-era5-single-levels',
+                    {
+                        'variable': variable,
+                        'year': YEARS[i:min(i+10,len(YEARS))],
+                        'area': AREA,
+                        'format': 'netcdf',
+                        'product_type': ['reanalysis'],  
+                        "download_format": "unarchived",    
+                        'month': [
+                            '01', '02', '03',
+                            '04', '05', '06',
+                            '07', '08', '09',
+                            '10', '11', '12',
+                        ],
+                        'day': [
+                            '01', '02', '03',
+                            '04', '05', '06',
+                            '07', '08', '09',
+                            '10', '11', '12',
+                            '13', '14', '15',
+                            '16', '17', '18',
+                            '19', '20', '21',
+                            '22', '23', '24',
+                            '25', '26', '27',
+                            '28', '29', '30',
+                            '31',
+                        ],
+                        'time': [
+                            '00:00', '01:00', '02:00',
+                            '03:00', '04:00', '05:00',
+                            '06:00', '07:00', '08:00',
+                            '09:00', '10:00', '11:00',
+                            '12:00', '13:00', '14:00',
+                            '15:00', '16:00', '17:00',
+                            '18:00', '19:00', '20:00',
+                            '21:00', '22:00', '23:00',
+                        ]
+                    },
+                    f"{variable_name}_part{i//10 + 1}.nc"
+                )
+            
+            #Change chunking to make it faster
+            os.system(f"nccopy -d0 -s {variable_name}_part{i//10 + 1}.nc u_{variable_name}_part{i//10 + 1}.nc")
+            os.system(f"ncks --cnk_plc=g2d --cnk_dmn time,365 u_{variable_name}_part{i//10 + 1}.nc m_{variable_name}_part{i//10 + 1}.nc")
+            os.system(f"rm u_{variable_name}_part{i//10 + 1}.nc")
+
+            #os.system(f"mv m_{variable_name}_part{i//10 + 1}.nc {variable_name}_part{i//10 + 1}.nc")
             
     os.chdir(ROOT_FOLDER)
 
             
 # A function to join all the nc files
-def join_files(variable):
-    print(f"Joining the data for variable \033[92m{variable}\033[0m")
+def join_files(variable_name):
+    print(f"Joining the data for variable \033[92m{variable_name}\033[0m")
     os.chdir(ROOT_FOLDER)
-    variable_name = VARIABLES.get(variable).get("reanalysis_name")
     #If the file already exists, remove it
-    if os.path.exists(f"data/reanalysis/reanalysis-{variable_name}/{variable_name}.nc"):
-        os.remove(f"data/reanalysis/reanalysis-{variable_name}/{variable_name}.nc")
-    os.chdir(f"{ROOT_FOLDER}/data/reanalysis/reanalysis-{variable_name}")
-    os.system(f"cdo -b F64 mergetime *.nc {variable_name}.nc")
-    os.chdir(ROOT_FOLDER)
+    if not os.path.exists(f"data/reanalysis/reanalysis-{variable_name}/{variable_name}.nc"):
+        #os.remove(f"data/reanalysis/reanalysis-{variable_name}/{variable_name}.nc")
+        os.chdir(f"{ROOT_FOLDER}/data/reanalysis/reanalysis-{variable_name}")
+        os.system(f"cdo -v -f nc4c -z zip_4 mergetime m_*.nc {variable_name}.nc")
+        os.system(f"rm m_*.nc")
+        os.chdir(ROOT_FOLDER)
+
+def wind_transform():
+
+    print("Transforming the wind data")
+
+    # Load the NetCDF file
+    u10 = xr.open_dataset(f'data/reanalysis/reanalysis-u10/u10.nc')
+    v10 = xr.open_dataset(f'data/reanalysis/reanalysis-v10/v10.nc')
+
+    # Extract the variables (assuming they are named 'u' and 'v')
+    u = u10['u10']
+    v = v10['v10']
+
+    # Compute the Euclidean norm
+    norm = np.sqrt(u**2 + v**2)
+
+    # Create a new dataset to store the norm
+    norm_ds = xr.Dataset({'u10': norm})
+
+	# Save the result to a new NetCDF file
+    norm_ds.to_netcdf(f'data/reanalysis/reanalysis-u10/w10.nc')
 
 #A function to summarize all the spatial data on a single point, also generate a csv file
 def summarize_data(variable):
     print(f"Summarizing the spatial data into a single point for variable \033[92m{variable}\033[0m")
     os.chdir(ROOT_FOLDER)
-    variable_name = VARIABLES.get(variable).get("reanalysis_name")
-    variable_cmip6_name = VARIABLES.get(variable).get("cmip6_name")
+    variable_name = VARIABLES[variable]["reanalysis_name"][0][1]
+   # variable_cmip6_name = VARIABLES.get(variable).get("cmip6_name")
+    
     os.chdir(f"{ROOT_FOLDER}/data/reanalysis/reanalysis-{variable_name}")
     data_nc = xr.open_dataset(f"{variable_name}.nc")
     
@@ -135,14 +166,14 @@ def summarize_data(variable):
             f"Time has the wrong size: {len(reanalysis)}, when it should be {days_between}"
     
     if(variable_name == "mx2t"):
-        reanalysis[variable_cmip6_name] = data_nc[variable_name].max(dim=["latitude","longitude"])
+        reanalysis[variable] = data_nc[variable_name].max(dim=["latitude","longitude"])
     elif(variable_name == "mn2t"):
-        reanalysis[variable_cmip6_name] = data_nc[variable_name].min(dim=["latitude","longitude"])
+        reanalysis[variable] = data_nc[variable_name].min(dim=["latitude","longitude"])
     else:
-        reanalysis[variable_cmip6_name] = data_nc[variable_name].mean(dim=["latitude","longitude"]) 
+        reanalysis[variable] = data_nc[variable_name].mean(dim=["latitude","longitude"]) 
     
     reanalysis.to_csv(f"{variable_name}.csv", index=False)
-    os.remove(f"{variable_name}.nc") #drop the nc file to realese space
+#    os.remove(f"{variable_name}.nc") #drop the nc file to realese space
     os.chdir(ROOT_FOLDER)
 
 
@@ -186,16 +217,23 @@ def main():
 
 #    Download the data for each variable
     for cmip_variable in VARIABLES:
-        for reanalysis_name in VARIABLES[cmip_variable]["reanalysis_name"]:
-            try:
-                download_data(reanalysis_name[0], reanalysis_name[1])
-                # join_files(variable)
-                # summarize_data(variable)
+    #    for reanalysis_name in VARIABLES[cmip_variable]["reanalysis_name"]:
+        #     try:
+        #         download_data(reanalysis_name[0], reanalysis_name[1])
+        #         join_files(reanalysis_name[1])
 
-            except Exception as e:
-                print(f"\033[91mError with variable {reanalysis_name[0]}: {e}\033[0m")
-    
-#    merge_all_data()
+        #     except Exception as e:
+        #         print(f"\033[91mError with variable {reanalysis_name[0]}: {e}\033[0m")
+
+        # if(cmip_variable == "sfcWind"):
+        #     wind_transform()
+
+        try:
+            summarize_data(cmip_variable)
+        except Exception as e:
+            print(f"\033[91mError with variable {cmip_variable[0]}: {e}\033[0m")
+
+    merge_all_data()
 #    final_dataset()
 
 if __name__ == "__main__":
--- code/sun_data.py
+++ code/sun_data.py
@@ -1,9 +1,63 @@
-AREA = [-30, -59, -35, -53] #If we want to do the Salto Grande area we need to set the invervals to  [-26,-60,-36,-48]
-YEARS = range(1980,2101)
-
 from datetime import datetime, timedelta
 import pandas as pd
 import pvlib
+import yaml 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+
+YEARS = []
+AREA = []
+
+
+#Given the lat and lon get the height of the location. I used suncalc since it was the reference for the daylight duration.
+def get_altitude(lat, lon):
+
+    # Dummy datetime
+    date_time = "2024.01.01/03:00"
+
+    # Construct the URL for SunCalc
+    url = f"https://www.suncalc.org/#/{lat},{lon},3/{date_time}/1/0"
+    
+    options = Options()
+    options.add_argument("--headless")
+    service = Service("/usr/bin/geckodriver")  # Replace with the path to your geckodriver
+    driver = webdriver.Firefox(service=service, options=options)
+    
+    # Load the page
+    driver.get(url)
+
+    # Wait for the page to load completely
+    WebDriverWait(driver, 10).until(
+        lambda d: d.execute_script("return document.readyState") == "complete"
+    )    
+    
+    # Wait for the page to load and locate the elevation element
+    elevation_element = driver.find_element(By.XPATH, "//*[@id='hoehe']") 
+    
+    elevation = elevation_element.text
+    
+    print(elevation)
+    
+    #Strip the "m" from the elevation
+    elevation = elevation.replace("m", "")
+    elevation = float(elevation) #Transform it to float
+    return elevation
+
+
+def load_configuration():
+    global YEARS, AREA
+
+    with open("code/conf.yml", 'r') as file:
+        conf = yaml.safe_load(file)
+    
+    YEARS = list(range(conf["REANALYSIS_YEARS"]["START"], conf["PROJECTION_YEARS"]["END"] + 1))
+    AREA = conf["AREA"]
+
+    return conf
+
 
 def sun_position(time):
     
@@ -17,15 +71,13 @@ def sun_position(time):
     return elevation, azimuth
 
 
-def amount_of_solar_hours_in_seconds(time):
-    print(time)
-    latitude = (AREA[0] + AREA[2]) / 2
-    longitude = (AREA[1] + AREA[3]) / 2
-    
+def amount_of_solar_hours_in_seconds(time, lat, lon, h):    
     times = pd.date_range(time, time + timedelta(days=1), freq='1s')
 
+    print(f"Getting daylight hours for {time} (lat:{lat}, lon:{lon}, h: {h})")
+
     # Calculate the solar position
-    solar_position = pvlib.solarposition.get_solarposition(times, latitude, longitude, altitude=141)
+    solar_position = pvlib.solarposition.get_solarposition(times, lat, lon, altitude=h)
 
     # Determine when the sun is above the horizon (altitude > 0)
     daylight_hours = solar_position[solar_position['apparent_elevation'] > 0]
@@ -41,17 +93,21 @@ def main():
     start = datetime(YEARS[0], 1, 1, 0, 0, 0)
     end = datetime(YEARS[-1], 12, 31, 23, 0, 0)
     
-    time_range = pd.date_range(start=start, end=end, freq="H")
-    elevation,azimuth = sun_position(time_range)
-    output = pd.DataFrame({"time": time_range, "elevation": elevation, "azimuth": azimuth})
-    output.to_csv("data/external_data/sun.csv", index=False)
+    # time_range = pd.date_range(start=start, end=end, freq="H")
+    # elevation,azimuth = sun_position(time_range)
+    # output = pd.DataFrame({"time": time_range, "elevation": elevation, "azimuth": azimuth})
+    # output.to_csv("data/external_data/sun.csv", index=False)
 
-    # #Get frequence at day scale
+    # Get daylight hours for each day
+    lat = (AREA[0] + AREA[2]) / 2
+    lon = (AREA[1] + AREA[3]) / 2
+    h = get_altitude(lat, lon)    
     time_range = pd.date_range(start=start, end=end, freq="D")
-    daylight_seconds = [amount_of_solar_hours_in_seconds(time) for time in time_range]
+    daylight_seconds = [amount_of_solar_hours_in_seconds(time, lat, lon, h) for time in time_range]
     output = pd.DataFrame({"time": time_range, "daylight_seconds": daylight_seconds})
     output.to_csv("data/external_data/daylight.csv", index=False)
 
 if __name__ == "__main__":
+    load_configuration()
     main()
    
