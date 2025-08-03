ac5fd26aa66008f6199089774507a34c003271c4
bruno tancredi <brunotancredi00@gmail.com>
2025-08-03 15:13:29 -0300

    small change

--- code/LSTMDownscaler.py
+++ code/LSTMDownscaler.py
@@ -276,10 +276,6 @@ class LSTMDownscaler():
                 
                 best_hps = tuner.get_best_hyperparameters()[0]
                 lstm = tuner.hypermodel.build(best_hps)
-
-                print(X_train.shape)  # Should match (num_samples, time_steps, expected_features)
-                print(X_valid.shape)  # Should match (num_samples, time_steps, expected_features)
-
                 lstm.fit(X_train, 
                          y_train, 
                          epochs=100 if VARIABLES[variable_name]["daily"] else 200, 
